c6835f1cbec3d80c0a90040d644dc76472d2e67e
Thomas-Toulouse <thomas_toulouse@yahoo.com>
2022-07-21 21:35:59 -0400

    update build

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CamViewerRtsp-Dev',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='thomas',
+    author_email='',
+    description=''
+)
