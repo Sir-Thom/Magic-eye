b9a920566b23b1d6b0d689569a893346bb8ebec0
Thomas-Toulouse <thomas_toulouse@yahoo.com>
2022-07-24 15:50:24 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
 #/usr/bin/sh
-# pyinstaller main.py -F \
-# --name "CamViewerRtsp" \
-# --add-data="host.py:data" \
-# --add-data="server.py:data" \
-# --add-data="settings.py:data" \
-# --clean \c
 echo "installing Camip..."
+chmod +x Camip.desktop
 cd Camip
 chmod +x *.py
 cd ..
@@ -14,19 +9,18 @@ cp -a Camip/  ~/.local/bin
 cp Camip.desktop ~/.local/share/applications
 echo -n "Do you want Camip on your desktop? [y/n]"
 read
-         if [[ $REPLY == "y" || $REPLY ==  "Y" ]]; then
+         if [[ $REPLY == "y" || $REPLY ==  "Y"  || $REPLY == "yes" ]]; then
                  cp Camip.desktop ~/Desktop
                  alias camip='python /usr/bin/camip.py' >> ~/.bashrc
                  echo "installation completed"  
                  break
          fi
-         if [[  $REPLY == "n" || $REPLY == "N" ]];then
+         if [[  $REPLY == "n" || $REPLY == "N" || $REPLY == "no" ]];then
                 alias camip='python /usr/bin/camip.py' >> ~/.bashrc
                 echo "installation completed"  
                 exit
          fi
-        if [[ $REPLY != "y" || $REPLY != "Y" || $REPLY != "n" || $REPLY != "N" ]];then
+        if [[ $REPLY != "y" || $REPLY != "Y" || $REPLY != "yes" || $REPLY != "n" || $REPLY != "N" || $REPLY == "no" ]];then
                 echo -e "\e[0;31m"Error:Invalid input.  " \e[m""Do you want Camip on your desktop? [y/n]"
                 read
                 fi
-#alias camip='python /usr/bin/camip.py' >> ~/.bashrc
