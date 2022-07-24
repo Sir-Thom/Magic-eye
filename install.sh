d09e7c8a8a7b3a5037b761cd2476964ba5e36325
Thomas-Toulouse <thomas_toulouse@yahoo.com>
2022-07-24 14:45:34 -0400

    update install script

--- install.sh
+++ install.sh
@@ -5,10 +5,28 @@
 # --add-data="server.py:data" \
 # --add-data="settings.py:data" \
 # --clean \c
+echo "installing Camip..."
 cd Camip
 chmod +x *.py
 cd ..
 sudo  cp -a Camip/.  /usr/bin
-cp -a Camip/.  ~/.local/bin
-#camviewer=sudo python /usr/local/bin/main.py >> ~/.bash_aliases
-alias camip='python /usr/bin/camip.py' >> ~/.bashrc
+cp -a Camip/  ~/.local/bin
+cp Camip.desktop ~/.local/share/applications
+echo -n "Do you want Camip on your desktop? [y/n]"
+read
+         if [[ $REPLY == "y" || $REPLY ==  "Y" ]]; then
+                 cp Camip.desktop ~/Desktop
+                 alias camip='python /usr/bin/camip.py' >> ~/.bashrc
+                 echo "installation completed"  
+                 break
+         fi
+         if [[  $REPLY == "n" || $REPLY == "N" ]];then
+                alias camip='python /usr/bin/camip.py' >> ~/.bashrc
+                echo "installation completed"  
+                exit
+         fi
+        if [[ $REPLY != "y" || $REPLY != "Y" || $REPLY != "n" || $REPLY != "N" ]];then
+                echo -e "\e[0;31m"Error:Invalid input.  " \e[m""Do you want Camip on your desktop? [y/n]"
+                read
+                fi
+#alias camip='python /usr/bin/camip.py' >> ~/.bashrc
